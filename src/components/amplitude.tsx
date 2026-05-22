e66d8decc86f14fd2a239ecccbc009c414aa5965
Adrien Cardinale <adrien.cardinale@heig-vd.ch>
2026-05-22 10:15:31 +0200

    add status ping in amplitude

--- src/components/amplitude.tsx
+++ src/components/amplitude.tsx
@@ -89,8 +89,14 @@ export default function Amplitude() {
     return (
         <>
             {status && (
-                <Badge variant="secondary" className="ml-auto">
-                    Amplitude du pendule: {amplitude !== null ? (amplitude/1000).toFixed(3) : "---"} m
+                <Badge variant="secondary" className="ml-auto flex items-center gap-2">
+                    Amplitude du pendule: {amplitude !== null ? (amplitude / 1000).toFixed(3) : "---"} m
+                    {amplitude !== null && (
+                    <span className="relative flex size-2">
+                        <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75" />
+                        <span className="relative inline-flex size-2 rounded-full bg-green-500" />
+                    </span>
+                    )}
                 </Badge>
             )}
         </>
